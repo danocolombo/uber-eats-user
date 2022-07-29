865d0c7be9e6e9dd8bbf5731c8ae5fb01e16037a
Dano Colombo <danocolombo@gmail.com>
2022-07-29 17:56:34 -0400

    day1

---
+++ src/screens/RestaurantDetailsScreen/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+    page: {
+        flex: 1,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 5 / 3,
+    },
+    title: { fontSize: 35, fontWeight: '600' },
+    subtitle: { fontSize: 15, color: '#525252' },
+    container: { margin: 10 },
+    iconContainer: {
+        position: 'absolute',
+        top: 40,
+        left: 10,
+    },
+    menuTitle: {
+        marginVertical: 20,
+        fontSize: 18,
+        letterSpacing: 0.7,
+    },
+});
