fa14fb726d57789d4746c0c4a8855f61750c964f
Dano Colombo <danocolombo@gmail.com>
2022-07-30 07:28:47 -0400

    restaurant and dishes displayed from Datastore

--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -1,25 +1,44 @@
-import { View, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import DishListItem from '../../components/DishListItem';
-import restaurants from '../../../assets/data/restaurants.json';
+
 import Header from './Header';
 import styles from './styles';
 import { useRoute, useNavigation } from '@react-navigation/native';
-
-const restaurant = restaurants[0];
+import { DataStore } from 'aws-amplify';
+import { Restaurant, Dish } from '../../models';
 
 const RestaurantDetailsPage = () => {
     const route = useRoute();
     const navigation = useNavigation();
-
     const id = route.params?.id;
-    console.warn(id);
+    // console.warn(id);
+    const [restaurant, setRestaurant] = useState(null);
+    const [dishes, setDishes] = useState([]);
+
+    useEffect(() => {
+        // get restaurant using id
+        if (id) {
+            DataStore.query(Restaurant, id).then(setRestaurant);
+            DataStore.query(Dish, (dish) => dish.restaurantID('eq', id)).then(
+                setDishes
+            );
+        } else {
+            return;
+        }
+    }, []);
+    console.log('restaurant\n:', restaurant);
+    console.log('dishes:\n', dishes);
+    if (!restaurant) {
+        return <ActivityIndicator size={'large'} color='gray' />;
+    }
 
     return (
         <View style={styles.page}>
             <FlatList
                 ListHeaderComponent={() => <Header restaurant={restaurant} />}
-                data={restaurant.dishes}
+                data={dishes}
                 renderItem={({ item }) => <DishListItem dish={item} />}
                 keyExtractor={(item) => item.name}
             />
